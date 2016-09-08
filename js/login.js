8570a77df38190922106b89853b3ca1a4dc01faf
NadeeSansari <nadeesenavirathna@gmail.com>
2016-09-08 09:31:55 +0530

    update all files

--- js/login.js
+++ js/login.js
@@ -3,9 +3,9 @@
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event)
+window.onclick = function(event) {
     if (event.target == modal) {
-    modal.style.display = "none"
+        modal.style.display = "none";
     }
-
+}
 </script>
